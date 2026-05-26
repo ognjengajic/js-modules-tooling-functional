25d37a3068db307988f4815193ea345db45dd409
ognjengajic <ognjeng535@gmail.com>
2026-05-26 12:06:59 +0200

    Top-Level await (ES2022)

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ add(`apples`, 1);
 add(`bread`, 5);
 
 console.log(cart);
+
+/*
+console.log(`start fetching`);
+const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+const data = await res.json();
+
+console.log(data);
+console.log(`Something`);
+*/
+
+const getLastPost = async function () {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(getLastPost);
+
+//not very clean
+//lastPost.then(last => console.log(last));
+
+//top level await
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
