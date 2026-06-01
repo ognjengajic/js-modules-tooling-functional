7503e86230dde2dc0c89b3b1f832bcf2a1a4c565
ognjengajic <ognjeng535@gmail.com>
2026-06-01 12:20:07 +0200

    Fixing Some Bad Code

--- clean.js
+++ clean.js
@@ -1,4 +1,4 @@
-var budget = [
+const budget = [
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -9,56 +9,66 @@ var budget = [
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ];
 
-var limits = {
+const spendingLimits = {
   jonas: 1500,
   matilda: 100,
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
+const addExpense = function (value, description, user = `jonas`) {
+  //if (!user) user = 'jonas';
   user = user.toLowerCase();
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
+  /*
+  let lim;
+  if (spendingLimits[user]) {
+    lim = spendingLimits[user];
   } else {
     lim = 0;
   }
+*/
 
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
+  //const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+  //const limit = getLimit(user);
+
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
   }
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
+const checkExpenses = function () {
+  for (const entry of budget)
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+  /*
+    let lim;
+    
+    if (spendingLimits[entry.user]) {
+      lim = spendingLimits[entry.user];
     } else {
       lim = 0;
     }
-
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
+*/
+  //const limit = spendingLimits?.[entry.user] ?? 0;
 };
-check();
+checkExpenses();
 
-console.log(budget);
-
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
+const logBigExpenses = function (bigLimit) {
+  let output = '';
+  for (const entry of budget)
+    output +=
+      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+  /*
+    if (entry.value <= -bigLimit) {
+      output += `${entry.description.slice(-2)} / `; // Emojis are 2 chars
     }
-  }
+      */
+
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+console.log(budget);
+logBigExpenses(500);
